03946a4dcef387c680eaf8e65841a5f793c781eb
akbarahmedjonov <teamender1991@gmail.com>
2026-04-06 19:11:33 +0500

    added stow

--- configuration.nix
+++ configuration.nix
@@ -1,21 +1,98 @@
+{ pkgs, ... }: 
+
 {
-  pkgs,
-  ...
-}: {
-  imports = [
-    ./hardware-configuration.nix
-  ];
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+  # --- 1. BOOT & SYSTEM BASE ---
+  imports = [ ./hardware-configuration.nix ];
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+    firewall.enable = false;
+  };
+
   time.timeZone = "Asia/Tashkent";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "25.11";
+
+  # --- 2. GRAPHICS & HARDWARE ---
+  hardware = {
+    graphics.enable = true;
+    bluetooth.enable = true;
+    
+    nvidia = {
+      open = false; # Set to true if using a Turing+ GPU (16xx/20xx or newer)
+      modesetting.enable = true;
+      powerManagement.enable = true;
+
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true; 
+        };
+        intelBusId = "PCI:0@0:2:0";
+        nvidiaBusId = "PCI:2@0:0:0";
+      };
+    };
+  };
+
+  # --- 3. SERVICES (AUDIO, DBUS, POWER) ---
+  services = {
+    # Pipewire Audio
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+    # Xserver & Display (Nvidia drivers live here)
+    xserver = {
+      enable = true;
+      videoDrivers = [ "modesetting" "nvidia" ];
+    };
+
+    # System Utilities
+    dbus.enable = true;
+    upower.enable = true;
+    gvfs.enable = true;
+    tumbler.enable = true;
+    power-profiles-daemon.enable = true;
+
+    # Login & Autologin
+    getty.autologinUser = "user";
+    
+    logind.settings.Login = {
+      HandleSuspendKey = "ignore";
+      HandleSuspendKeyLongPress = "ignore";
+    };
+  };
+
+  # --- 4. USER CONFIGURATION ---
+  users.users.user = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "audio" "video" "input" ];
+    shell = pkgs.fish; # Correct way to set default shell
+    packages = with pkgs; [ tree ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8WDMwSqIpZRgL18utzyZmH5c4yXQ8x7WC0nQz41MmV"
+    ];
+  };
+
+  # --- 5. PROGRAMS & PORTALS ---
   programs = {
     niri.enable = true;
     xwayland.enable = true;
     fish.enable = true;
     ssh.startAgent = true;
   };
+
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
@@ -25,110 +102,29 @@
     ];
   };
 
-  ## Security
-
+  # --- 6. SECURITY ---
   security = {
     polkit.enable = true;
     rtkit.enable = true;
   };
 
-  services = {
-    ## Audio
-
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-    };
-
-    ## Intel and nvidia graphics drivers
-
-    xserver = {
-      enable = true;
-      videoDrivers = [
-        "modesetting"
-        "nvidia"
-      ];
-    };
-
-    ## Other services
-
-    gnome.gcr-ssh-agent.enable = false;
-    dbus.enable = true;
-#    displayManager= {
-#      sddm = {
-#        enable=true;
-#	settings = {
-#          Autologin = {
-#            Session = "niri.desktop";
-#	    User = "user";
-#	  };
-#	};
-#      };
-#    };
-    getty.autologinUser = "user";
-    upower.enable = true;
-    gvfs.enable = true;
-    power-profiles-daemon.enable = true;
-    tumbler.enable = true;
-    logind.settings.Login = {
-      enable = true;
-      HandleSuspendKey = "ignore";
-      HandleSuspendKeyLongPress = "ignore";
-    };
-  };
-  users.users.user = {
-    isNormalUser = true;
-    extraGroups = ["wheel" "networkmanager" "audio" "video" "input"];
-    shell = pkgs.fish;
-    packages = with pkgs; [
-      tree
-    ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8WDMwSqIpZRgL18utzyZmH5c4yXQ8x7WC0nQz41MmV"
-    ];
-  };
-  hardware.bluetooth.enable = true;
-  networking.firewall.enable = false;
+  # --- 7. SYSTEM-WIDE PACKAGES ---
   environment.systemPackages = with pkgs; [
-    starship
-    zoxide
-    vim
-    wget
-    curl
-    waybar
-    git
-    swaybg
-    home-manager
-    fuzzel
+    # Essentials
+    vim wget curl git home-manager
+    # Terminal UI
+    starship zoxide
+    # Wayland / UI Tools
+    waybar swaybg fuzzel mesa-demos
+    # Hardware Info
     pciutils
-    mesa-demos
   ];
+
+  # --- 8. FONTS ---
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-color-emoji
   ];
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  system.stateVersion = "25.11";
-  nixpkgs.config.allowUnfree = true;
-  hardware = {
-    graphics.enable = true;
-    nvidia = {
-      open = false;
-      modesetting.enable = true;
-      powerManagement.enable = true;
-    };
-    nvidia.prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true; # gives you `nvidia-offload`
-      };
-      intelBusId = "PCI:0@0:2:0";
-      nvidiaBusId = "PCI:2@0:0:0";
-    };
-  };
 }
